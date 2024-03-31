65a620e9f0d01e3b70116a41793a8e55d96e2c83
Sorrawich Yooboon (Ko) <sorrawich.y@gmail.com>
2024-03-31 23:35:30 +0700

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/sorrawichYooboon/eguslibmodel
+
+go 1.20
+
+require github.com/sorrawichYooboon/sgslibmodel v1.0.1 // indirect
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	calculator "github.com/sorrawichYooboon/sgslibmodel"
+)
+
+func main() {
+	fmt.Println("Hello, World!")
+	a := calculator.CalSquareArea(5)
+	fmt.Println(">>> Square Area: ", a)
+}
