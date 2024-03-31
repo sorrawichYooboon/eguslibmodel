ed7f2cfe69eea1e887a3a9269c94cbc2c730c1ad
Sorrawich Yooboon (Ko) <sorrawich.y@gmail.com>
2024-03-31 23:47:28 +0700

    feat:lib numutil

--- main.go
+++ main.go
@@ -3,11 +3,14 @@ package main
 import (
 	"fmt"
 
-	calculator "github.com/sorrawichYooboon/sgslibmodel"
+	calculator "github.com/sorrawichYooboon/sgslibmodel/calculator"
+	numutil "github.com/sorrawichYooboon/sgslibmodel/numutil"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
 	a := calculator.CalSquareArea(5)
 	fmt.Println(">>> Square Area: ", a)
+
+	b := numutil.FindMax([]int{1, 2, 3, 4, 5})
+	fmt.Println(">>> Max: ", b)
 }
